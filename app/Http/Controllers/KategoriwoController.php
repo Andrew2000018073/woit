10b21c00318f4d83453b4dc4e549e39e8afaa13d
Andrew2000018073 <andrew2000018073@webmail.uad.ac.id>
2023-11-20 12:58:34 +0700

    update policies

---
+++ app/Http/Controllers/KategoriwoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\kategoriwo;
+use App\Http\Requests\StorekategoriwoRequest;
+use App\Http\Requests\UpdatekategoriwoRequest;
+
+class KategoriwoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorekategoriwoRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorekategoriwoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\kategoriwo  $kategoriwo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(kategoriwo $kategoriwo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\kategoriwo  $kategoriwo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(kategoriwo $kategoriwo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatekategoriwoRequest  $request
+     * @param  \App\Models\kategoriwo  $kategoriwo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatekategoriwoRequest $request, kategoriwo $kategoriwo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\kategoriwo  $kategoriwo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(kategoriwo $kategoriwo)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/WorkorderController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\workorder;
+use App\Http\Requests\StoreworkorderRequest;
+use App\Http\Requests\UpdateworkorderRequest;
+
+class WorkorderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreworkorderRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreworkorderRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\workorder  $workorder
+     * @return \Illuminate\Http\Response
+     */
+    public function show(workorder $workorder)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\workorder  $workorder
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(workorder $workorder)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateworkorderRequest  $request
+     * @param  \App\Models\workorder  $workorder
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateworkorderRequest $request, workorder $workorder)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\workorder  $workorder
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(workorder $workorder)
+    {
+        //
+    }
+}
